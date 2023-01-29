1cf69df051d3ff9ccf5f81ae273368df29b9de39
cakosik <115614081+cakosik@users.noreply.github.com>
2023-01-29 22:46:46 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,26 @@
+
+tokens = ['5983585519:AAGwXXvjSVkcKXtzBEDCq1CvxcguX5gZyKA'] # 1 -@Minko_Game_Bot
+token = tokens[0]
+QiwiP2P_token = 'eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6IjFyc3QweS0wMCIsInVzZXJfaWQiOiI5OTY3MDU0NDkxMTEiLCJzZWNyZXQiOiIzMDMyNGQwMGEyMDgwOTA5NDlhMTk2OTIxNDJkNThlMGVkMmM5MDZhMGQ1MTVkYzIzOTliMTVhNGFiN2Y5OGE0In19'
+
+owner_id = 1001080135
+
+full_bot_name = 'RBK'
+bot_name = '@RBKgames_bot'
+channel_name = 'RBK_channel'
+chat_name = 'RBK_chat'
+
+start_money = 100000000
+donat_money = 20
+cash_family = 5
+
+
+
+owner = '<a href="https://t.me/CheIovechek06">CHESTER</a>'
+channel = '<a href="t.me/RBKchannelbot">RBK CHANNEL | Канал</a>'
+chat = '<a href="t.me/tmkinderbot">RBK | Игровой чат #1</a>'
+chat2 = '<a href="https://t.me/tmkinderbot">RBK | Игровой чат #2</a>'
+chat_id = -1001745253207 #  @qwechat  -1001745253207 | QWE ADMIN CHAT  -1001745253207 | @qwe_chat_two -1001745253207
+
+admin_chat_id = '-1001745253207'
+
